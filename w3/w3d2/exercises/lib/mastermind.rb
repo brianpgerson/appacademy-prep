f62d8e2a10214b7fba8f23d5e828d37110b4ce49
Brian Gerson <brianpgerson@gmail.com>
2015-12-24 23:46:30 -0800

    it's playable and fun. cool

--- w3/w3d2/exercises/lib/mastermind.rb
+++ w3/w3d2/exercises/lib/mastermind.rb
@@ -25,7 +25,8 @@ class Code
 		4.times do 
 			random_code << choices[Random.rand(6)]
 		end
-		Code.new(random_code)
+		random_pegs = random_code.join("").downcase
+		Code.new(random_pegs)
 	end
 
 	def initialize(pegs)
@@ -78,16 +79,58 @@ class Game
 		else 
 			@secret_code = secret_code
 		end
+		@turns_left = 10
 	end
 
 	def get_guess
-		input = $stdin
-		Code.new(input)
+		puts "Make a guess...if you dare. You have #{@turns_left} remaining."
+		puts "(You must be feeling nervous...)" if @turns_left == 2
+		puts "(Only one turn left, foolish human...)" if @turns_left == 1
+		input = gets.chomp
+		if input == "info"
+			puts instructions
+		end
+		unless input =~ /([rbgopy][rbgopy][rbgopy][rbgopy])/
+			puts "Please enter four colors. Your choices are RED, BLUE, GREEN, ORANGE, YELLOW, PURPLE."
+			input = gets.chomp
+		end
+		guess = Code.new(input)
 	end
 
 	def display_matches(guess)
-		puts "exact matches: #{secret_code.exact_matches(guess)}"
-		puts "near matches: #{secret_code.near_matches(guess)}"
+		puts "Your last guess was #{guess.pegs.split("")}. Here's how you did:"
+		puts "Exact matches: #{secret_code.exact_matches(guess)}"
+		puts "Near matches: #{secret_code.near_matches(guess)}\n"
+	end
+
+	def play_turn
+		guess = get_guess
+		if check_winner(guess)
+			@turns_left = 0
+		end
+		display_matches(guess)
+	end
+
+	def check_winner(guess)
+		if guess == @secret_code
+			puts "You've broken my code! Robert Knuth would be so proud."
+			true
+		end
+	end
+
+	def instructions
+		"I, as a superintelligent computing machine, have selected a code. That code consists of 4 colors (for example, red, blue, green, orange) in a particular order.\n\nThe possible colors are RED, BLUE, GREEN, ORANGE, YELLOW, and PURPLE. Your job is to guess the combination of colors that I have chosen.\n\nTo do so, enter a guess. To guess the example above, you'd type in 'rbgo'. Once you've guessed, I will tell you the number of exact matches — or correct colors in the correct positions — and near matches, which are the right colors in the wrong spots.\n\nIf my chosen code was 'brgo', the example you guessed would have two exact matches (green and orange), and two near matches (blue and red). The same color may be used more than once, so 'rrrr' is a valid guess.\n\nUse the clues to guess my code...and I will let you live. To display this information again, enter 'info'."
+	end
+
+	def play
+		puts instructions
+		while @turns_left > 0
+			play_turn
+			@turns_left -= 1
+		end
+		if @turns_left == 0
+			puts "It's all over. Your human soul now belongs to me! The correct code was #{secret_code.pegs.split("")}" 
+		end
 	end
 
 end
@@ -97,15 +140,5 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+ game = Game.new
+ game.play
